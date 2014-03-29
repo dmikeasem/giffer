74c5e542af93f8d20037e7d7647488a33bb076f5
dmikeasem <d.mike.asem@gmail.com>
2014-03-29 04:11:28 -0400

    User/Post Associations and Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329055804) do
+ActiveRecord::Schema.define(version: 20140329074654) do
 
   create_table "posts", force: true do |t|
     t.text     "body"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20140329055804) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "title"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
