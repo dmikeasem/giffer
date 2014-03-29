e6b4bb5dbd39407f6b1b46fe8317951a3bea9a51
dmikeasem <d.mike.asem@gmail.com>
2014-03-29 07:29:16 -0400

    Added usernames to sign_in and user/edit, validating presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts
+
+  validates :username, presence: true
+validates :username, uniqueness: true
 end
