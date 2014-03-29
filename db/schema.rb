a2d7da14db02e5a74f1c7456638e196a9ca272dc
Jeremy Meyer <jjman505@gmail.com>
2014-03-29 02:39:09 -0400

    schema change for unique username

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140329055804) do
     t.string   "username"
   end
 
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
