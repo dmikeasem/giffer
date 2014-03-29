7468f1e48a1969ee1d2826df910bf9358f1c303c
Jeremy Meyer <jjman505@gmail.com>
2014-03-29 13:17:53 -0400

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Giffer::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/signup"
-  resources :posts
-
-  devise_for :users
   root "posts#index"
-  get "about" => "pages#about"
+
+  get "pages/home"
+
+  resources :posts
+  devise_for :users
+
   get "posts" => "posts#index"
   get "app" => "pages#app"
-  # get "users/search"
+  get "apost" => "posts#show"
+
   get "users/:username" => "users#show"
+  # get "users/search"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
